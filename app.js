580898ef08ad7dab8f3803b4d2a5c001e8ad946f
Mike Ulvila <mikeulvila@me.com>
2016-02-25 13:34:38 -0600

    installed express, server running

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 'use strict';
 
+const express = require('express');
 const sqlite3 = require('sqlite3');
+
+const PORT = 3000;
+
+const app = express();
 const db = new sqlite3.Database('./db/chinook.sqlite');
+
 
 console.log('# of invoices per country');
 
-db.all(`
+app.get('/', (req, res) => {
+  db.all(`
   SELECT   COUNT(*) AS count,
            BillingCountry AS country
   FROM     Invoice
   GROUP BY BillingCountry
   ORDER BY count DESC`,
-    (err, res) => {
+    (err, data) => {
       if (err) throw err;
 
-      console.log(res);
+      res.send(data);
     }
-);
+  );
 
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
